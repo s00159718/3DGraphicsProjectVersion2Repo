35745d8396e4e5c01f8c3d391e88ce3a8833f0ae
James Craven <s00159718@mail.itsligo.ie>
2018-04-20 15:51:10 +0100

    Game1 class updated to match previous project version.

--- 3DGraphicsProjectVersion2/3DGraphicsProjectVersion2/Game1.cs
+++ 3DGraphicsProjectVersion2/3DGraphicsProjectVersion2/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Sample;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace _3DGraphicsProjectVersion2
 {
@@ -10,26 +13,61 @@ namespace _3DGraphicsProjectVersion2
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        InputEngine input;
+        DebugEngine debug;
+        ImmediateShapeDrawer shapeDrawer;
+
+        List<SimpleModel> gameObjects = new List<SimpleModel>();
+        Camera mainCamera;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 768;
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            graphics.ApplyChanges();
+
+            input = new InputEngine(this);
+            debug = new DebugEngine();
+            shapeDrawer = new ImmediateShapeDrawer();
+
+            IsMouseVisible = true;
             Content.RootDirectory = "Content";
         }
 
         protected override void Initialize()
         {
+            GameUtilities.Content = Content;
+            GameUtilities.GraphicsDevice = GraphicsDevice;
+            GameUtilities.Random = new System.Random();
+
+            debug.Initialize();
+            shapeDrawer.Initialize();
+
+            mainCamera = new Camera("cam", new Vector3(0, 20, 10), new Vector3(0, 0, -1));
+            mainCamera.Initialize();
+
             base.Initialize();
+        }
+
+        public void AddModel(CustomEffectModel Model)
+        {
+            Model.Initialize();
+            Model.LoadContent();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            AddModel(new PointLightModel("wolf", new Vector3(0, 2f, -32)));
         }
 
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+
         }
 
         protected override void Update(GameTime gameTime)
